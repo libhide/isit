a044e92bd59400ed3521276e3e9a54b3e6d80a34
Ratik Sharma <Ratik96@gmail.com>
2016-05-23 17:21:55 +0530

    Add markup and link up with JS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 // Functions
 
 function getTimeRemaining(endtime){
@@ -15,15 +16,18 @@ function getTimeRemaining(endtime){
   };
 }
 
-function initializeClock(id, endtime){
-  var clock = document.getElementById(id);
+function initializeClock(endtime){
+  var days = document.getElementById("days");
+  var hours = document.getElementById("hours");
+  var minutes = document.getElementById("minutes");
+  var seconds = document.getElementById("seconds");
   // Helper function
   function updateClock() {
     var t = getTimeRemaining(birthdayRaw);
-    clock.innerHTML = t.days + ' day(s) | ' +
-                      ('0' + t.hours).slice(-2) + ' hour(s) | ' +
-                      ('0' + t.minutes).slice(-2) + ' minute(s) | ' +
-                      ('0' + t.seconds).slice(-2) + ' sec(s)';
+    days.innerHTML = t.days;
+    hours.innerHTML = ('0' + t.hours).slice(-2);
+    minutes.innerHTML = ('0' + t.minutes).slice(-2);
+    seconds.innerHTML = ('0' + t.seconds).slice(-2);
     if(t.total <= 0) {
       clearInterval(timeinterval);
     }
@@ -46,28 +50,26 @@ function refreshAt(hours, minutes, seconds) {
   then.setSeconds(seconds);
 
   var timeout = (then.getTime() - now.getTime());
-  console.log("Setting alarm for ~" + Math.floor( (timeout/1000/60) % 60 ) + " minutes");
+  // console.log("Setting alarm for ~" + Math.floor( (timeout/1000/60) % 60 ) + " minutes");
   setTimeout(function() { window.location.reload(true); }, timeout);
 }
 
 /******** Logic *********/
 
-// Constants
-var BIRTHDAY_MONTH = "5";
-var BIRTHDAY_MONTH_STR = "May";
-var BIRTHDAY_DAY = "22";
-
 var now = new Date();
 var currentYear = now.getFullYear();
 
-var birthdayRaw = BIRTHDAY_MONTH_STR + ' ' + BIRTHDAY_DAY + ' ' + currentYear + ' ' + '23:59:59 GMT+05:30';
+var birthdayRaw = 'April 30' + ' ' + currentYear + ' ' + '23:59:59 GMT+05:30';
 var birthday = Date.parse(birthdayRaw);
 
 // If it's the birthday, don't show countdown, else show it
-if (now.getDate() == BIRTHDAY_DAY && now.getMonth() + 1 == BIRTHDAY_MONTH) {
-  // BIRTHDAY!!!!!! :D
-  var clock = document.getElementById("text");
-  clock.innerHTML = "HAPPY BIRTHDAY!";
+if (now.getDate() == 1 && now.getMonth() + 1 == 5) {
+  // BIRTHDAY! :D
+  var ans = document.getElementsByClassName("ans")[0];
+  ans.innerHTML = "Yes! It's his birthday! Go wish him!";
+
+  // Hide countdown
+  document.getElementById("countdown").style.display = 'none';
 
   // Need to set a refresh-alarm for when the clock strikes 00:00
   refreshAt(23, 59, 59) // Will refresh the page at 23:59:59
@@ -78,10 +80,10 @@ if (now.getDate() == BIRTHDAY_DAY && now.getMonth() + 1 == BIRTHDAY_MONTH) {
   var nowParsed = Date.parse(now);
   if (birthday < nowParsed) {
     currentYear += 1;
-    birthdayRaw = BIRTHDAY_MONTH_STR + ' ' + BIRTHDAY_DAY + ' ' + currentYear + ' ' + '23:59:59 GMT+05:30';
+    birthdayRaw = 'April 30' + ' ' + currentYear + ' ' + '23:59:59 GMT+05:30';
     birthday = Date.parse(birthdayRaw);
   }
 
   // init clock
-  initializeClock('text', birthdayRaw);
+  initializeClock(birthdayRaw);
 }
